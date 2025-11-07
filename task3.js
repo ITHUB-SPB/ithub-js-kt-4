5b7d2e3195c1e40afe1c0aef5344c4ec7f9cf772
SD N <yaromirgames60@gmail.com>
2025-11-07 13:45:39 +0300

    Add files via upload

---
+++ task3.js
@@ -0,0 +1,25 @@
+/**
+ * @param {Object[]} args
+ * @returns {Object}
+ *
+ * @description Функция принимает на вход неопределенное
+ * количество аргументов, каждый из которых является объектом
+ * и возвращает единый объект по образцу
+ *
+ * @example
+ * const firstObject = { id: 1 }
+ * const secondObject = { status: 'draft', created: "2025-05-05" }
+ * joinObjects(firstObject, secondObject) // { id: 1, status: "draft", created: "2025-05-05" }
+ */
+export function joinObjects(...args) {
+   
+    if (args.length === 0) {
+        throw new Error("Отсутствуют объекты для слияния");
+    }
+
+    if (args.length === 1) {
+        return { ...args[0] };
+    }
+
+    return Object.assign({}, ...args);
+}
