e91bfca09679d84517da9358976900a3a58d6eac
Igor Schwarz <igor12333214@gmail.com>
2025-11-08 13:26:25 +0300

    1 task

--- src/task1.js
+++ src/task1.js
@@ -1,19 +1,26 @@
 /**
  * @param {{"word": string}[]} wordObjects - Объекты со словами
- * 
+ *
  * @returns {undefined}
- * 
+ *
  * @throws Выбрасывает исключение, если вызвана без аргументов
- * 
+ *
  * @description Функция принимает на вход неопределенное количество
  * объектов с ключом `word` и выводит кумулятивную строку в консоль
  * по примеру
- * 
+ *
  * @example
  * logString({ word: "Hello" }, { word: "world!" }) // "Hello world!"
  * logString({ word: "Hi" }, { word: "my" }, { word: "name" }, { word: "is..." }) // "Hi my name is..."
  */
 export function logString(...wordObjects) {
+    if (wordObjects.length === 0) {
+      throw new Error("Отсутствуют объекты для вывода")
+    }
+    
+    const result = wordObjects.map((str) => Object.values(str).join(' ')).join(' ')
 
-  return
+        
+    console.log(result)
+    return
 }
