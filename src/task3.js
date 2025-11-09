2ec28f681e40ae28ef0ebd6b0ed5a04aa9d3387c
Daniil76Glushkov <risekindoms8@gmail.com>
2025-11-09 22:44:05 +0300

    .

--- src/task3.js
+++ src/task3.js
@@ -10,20 +10,6 @@
  * splitName("Oliver") // { firstName: "Oliver" }
  */
 export function splitName(fullName) {
-    return
-}
-
-/**
- * @param {String[]} namesArray - Массив имён
- * @returns {{firstName: String, lastName?: String}[]}
- *
- * @description Функция принимает на вход массив имен и возвращает новый
- * массив, полученный их трансформацией функцией `splitName`
- *
- * @example
- * toSplittedNames(["John Al", "Oliver"]) // [{ firstName: "John", lastName: "Al" }, { firstName: "Oliver" }]
- */
-export function splitName(fullName) {
     const names = fullName.trim().split(' ').filter(Boolean);
     
     if (names.length === 1) {
@@ -34,4 +20,4 @@ export function splitName(fullName) {
         firstName: names[0],
         lastName: names.slice(1).join(' ')
     };
-}
+} 
